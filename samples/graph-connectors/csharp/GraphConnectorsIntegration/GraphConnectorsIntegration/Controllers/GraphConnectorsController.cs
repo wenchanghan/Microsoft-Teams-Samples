98994b767e57be769f4b27d1ab8a0ae33f70bd0d
Derrick Han <wenha@microsoft.com>
2022-06-07 18:06:34 -0700

    WIP

---
+++ samples/graph-connectors/csharp/GraphConnectorsIntegration/GraphConnectorsIntegration/Controllers/GraphConnectorsController.cs
@@ -0,0 +1,93 @@
+﻿namespace GraphConnectorsIntegration.Controllers
+{
+    using GraphConnectorsIntegration.Utilities;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Graph;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class GraphConnectorsController : ControllerBase
+    {
+        private const string AppId = "";
+        private readonly WebhookTokenValidator webhookTokenValidator;
+
+        public GraphConnectorsController()
+        {
+            this.webhookTokenValidator = new WebhookTokenValidator();
+        }
+
+        [HttpPost]
+        [Route("ProcessGraphWebhookChangeNotificationCollection")]
+        public async Task<IActionResult> ProcessGraphWebhookChangeNotificationCollectionAsync([FromBody] ChangeNotificationCollection changeNotificationCollection)
+        {
+            // The suggested way to handle Graph Webhook change notification is to leverage Web-Queue-Worker architecture. Queue the notification and immediately return an HTTP2xx.
+            // This sample app processes the entire change notification within the API call, purely for simplicity.
+
+            if (changeNotificationCollection?.Value?.Count() != 1
+                || changeNotificationCollection.ValidationTokens?.Count() != 1)
+            {
+                // Invalid payload. Discard.
+                return this.Ok();
+            }
+
+            string validationToken = changeNotificationCollection.ValidationTokens.Single();
+            ChangeNotification changeNotification = changeNotificationCollection.Value.Single();
+            string tenantIdFromNotification = changeNotification.TenantId?.ToString();
+
+            try
+            {
+                await this.webhookTokenValidator.ValidateToken(validationToken, tenantIdFromNotification, AppId, new CancellationToken());
+            }
+            catch
+            {
+                // Notification not from trusted source. Discard.
+                return this.Ok();
+            }
+
+            IDictionary<string, object> changeDetails = changeNotification.ResourceData?.AdditionalData;
+            string targetConnectorState = GetChangeDetailByName(changeDetails, "state");
+
+            if (!changeNotification.Resource.Contains("external/connectors"))
+            {
+                // Resource not in scope for processing. Discard.
+                return this.Ok();
+            }
+
+            // Validation completed. Process Graph Connector change.
+            // Ideally, this should be done in a worker/processor after ack'ing the Webhook notification call.
+            if (string.Equals("disabled", targetConnectorState, StringComparison.OrdinalIgnoreCase))
+            {
+                // Get all connections.
+                // Delete all connections.
+            }
+            else if (string.Equals("enabled", targetConnectorState, StringComparison.OrdinalIgnoreCase))
+            {
+                string connectorId = GetChangeDetailByName(changeDetails, "id");
+                string connectorTicket = GetChangeDetailByName(changeDetails, "connectorsTicket");
+
+                // Create connection.
+                // Create schema.
+                // Simulate data ingestions.
+            }
+            else
+            {
+                // Invalid target state. Discard.
+                return this.Ok();
+            }
+
+            return this.Ok();
+        }
+
+        private static string GetChangeDetailByName(IDictionary<string, object> changeDetails, string key)
+        {
+            object detailObject = null;
+            changeDetails?.TryGetValue(key, out detailObject);
+            return detailObject?.ToString();
+        }
+    }
+}
---
+++ samples/graph-connectors/csharp/GraphConnectorsIntegration/GraphConnectorsIntegration/Utilities/WebhookTokenValidator.cs
@@ -0,0 +1,62 @@
+﻿namespace GraphConnectorsIntegration.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.IdentityModel.Protocols;
+    using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+    using Microsoft.IdentityModel.Tokens;
+
+    public class WebhookTokenValidator
+    {
+        static readonly HashSet<string> ExpectedMicrosoftApps = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+        {
+            "56c1da01-2129-48f7-9355-af6d59d42766", // Graph Connector Service
+            "0bf30f3b-4a52-48df-9a82-234910c4a086", // Microsoft Graph Change Tracking
+        };
+
+        private readonly IConfigurationManager<OpenIdConnectConfiguration> configurationManager;
+
+        public WebhookTokenValidator()
+        {
+            this.configurationManager =
+                new ConfigurationManager<OpenIdConnectConfiguration>(
+                        $"https://login.microsoftonline.com/common/.well-known/openid-configuration",
+                        new OpenIdConnectConfigurationRetriever());
+        }
+
+        public async Task ValidateToken(string validationToken, string tenantId, string audience, CancellationToken cancellationToken)
+        {
+            OpenIdConnectConfiguration configuration = await this.configurationManager.GetConfigurationAsync(cancellationToken);
+
+            var handler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+
+            string issuer = configuration.Issuer.Replace("{tenantid}", tenantId, StringComparison.OrdinalIgnoreCase);
+
+            handler.ValidateToken(
+                validationToken,
+                new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience,
+                    ValidateLifetime = true,
+                    RequireSignedTokens = true,
+                    IssuerSigningKeys = configuration.SigningKeys,
+                },
+                out securityToken);
+
+            JwtSecurityToken jwtSecurityToken = (JwtSecurityToken)securityToken;
+            string appId = (string)jwtSecurityToken.Payload["appid"];
+
+            if (!ExpectedMicrosoftApps.Contains(appId))
+            {
+                throw new SecurityTokenException("The token is not generated from expected Microsoft applications");
+            }
+        }
+    }
+}
